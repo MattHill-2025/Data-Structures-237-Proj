58d5f056b0117c37fd7f700e12bf5bf39594f32b
MattHill-2025 <matt.hill.1500@gmail.com>
2023-11-28 15:42:41 -0500

    Doubly-Linked List Node definition

---
+++ Node.h
@@ -0,0 +1,28 @@
+// File: Node.h
+// Doubly-linked list node definition/implementation
+/** \file Node.h
+ * \brief Creates a node with a previous and next pointer which will be implemented in DblLink.h
+ */
+
+
+#ifndef NODE_H
+#define NODE_H
+
+//using namespace std;
+
+// Need to prototype template classes if they are to be friends
+template <class eltType> class DblLink;
+template <class eltType> class DblLinkIterator;
+
+template <class eltType> class Node
+{       private:
+                Node(eltType info, Node *pLink = 0, Node *rLink=0)
+                                   : data(info), prev(pLink), next(rLink) {};
+                eltType data;
+                Node*   prev;
+                Node*   next;
+        friend class DblLink<eltType>;
+        friend class DblLinkItr<eltType>;
+};
+
+#endif
