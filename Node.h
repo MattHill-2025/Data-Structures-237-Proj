2dd43e5437f80bf7856148232a8ca5d64efcc312
MattHill-2025 <matt.hill.1500@gmail.com>
2024-02-22 20:06:27 -0500

    Update Node.h

--- Node.h
+++ Node.h
@@ -15,14 +15,15 @@ template <class eltType> class DblLink;
 template <class eltType> class DblLinkIterator;
 
 template <class eltType> class Node
-{       private:
-                Node(eltType info, Node *pLink = 0, Node *rLink=0)
-                                   : data(info), prev(pLink), next(rLink) {};
-                eltType data;
-                Node*   prev;
-                Node*   next;
-        friend class DblLink<eltType>;
-        friend class DblLinkItr<eltType>;
+{	private:
+		Node(eltType info, Node *pLink = 0, Node *rLink=0) 
+				   : data(info), prev(pLink), next(rLink) {};
+		eltType data;
+		Node*	prev; 
+		Node*	next;
+	friend class DblLink<eltType>;
+	friend class DblLinkItr<eltType>;
 };
 
 #endif
+
